3f43c68cdbc333287678d08755dbd912aa36d208
Miguel Liberato <miguel.liberato.carmin@gmail.com>
2021-09-25 08:19:51 -0500

    commit

---
+++ ms-server-config/src/main/java/com/aforo255/exam/MsServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.aforo255.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServerConfigApplication.class, args);
+	}
+
+}
---
+++ ms-server-config/src/test/java/com/aforo255/exam/MsServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aforo255.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
